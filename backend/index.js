0ba5a41be357f79b2a80e81f76ebd100729c8431
piyush016 <piyushcool116@gmail.com>
2024-02-26 00:12:52 +0530

    Routes completed

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const { createTodo } = require("./types");
+const { todo } = require("./db");
 
 app.use(express.json());
 
-app.post("/todo", function (req, res) {
+app.post("/todo", async function (req, res) {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if (!parsedPayload.success) {
@@ -13,15 +14,37 @@ app.post("/todo", function (req, res) {
     });
     return;
   }
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+  res.json({
+    msg: "Success",
+  });
 });
 
-app.get("/todos", function (req, res) {});
+app.get("/todos", async function (req, res) {
+  const todos = await todo.find();
+  res.json({
+    todos,
+  });
+});
 
-app.put("/completed", function (req, res) {
+app.put("/completed", async function (req, res) {
   const updatePayload = req.body;
   const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
     res.status(404).json({ msg: "Invalid inputs" });
+    return;
   }
-  return;
+  await todo.update(
+    { _id: req.body.id },
+    {
+      completed: true,
+    }
+  );
+  res.json({
+    msg: "Completed",
+  });
 });
